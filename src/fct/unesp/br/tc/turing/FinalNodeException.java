aa6a2c2d0c6d92e38d935298d6fddef4de23fabc
Leonardo Dias <leonardodias914@gmail.com>
2016-09-22 19:55:52 -0300

    FinalNodeException adicionado

---
+++ src/fct/unesp/br/tc/turing/FinalNodeException.java
@@ -0,0 +1,10 @@
+package fct.unesp.br.tc.turing;
+
+public class FinalNodeException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4578543938738447212L;
+
+}
